d86e3c98a549fdfc0d6bb527ae10dcf83d273a91
Jorys Pephily <jorys.pephily@coda-student.school>
2024-12-10 14:46:44 +0100

    déplacement du css

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
 'use strict';
-'use strict';
 const Naval = function naval() {
     const elBody = document.querySelector('body');
-    elBody.innerHTML += `
-        <style>
-            body{
-                justify-content: center;
-                align-items: center;
-                display: flex;
-                flex-direction: column ;
-            }
-            td {
-                border: black 1px solid;
-                background-color: green;
-                text-align: center;
-                width: 20px;
-                height: 20px;
-            }
-        </style>
-    `;
+    elBody.innerHTML += ``;
     let tableHTML = '<table>';
 
     for (let i = 1; i <= 10; i += 1) {
