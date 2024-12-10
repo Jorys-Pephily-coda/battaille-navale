ea8ee15bfe28c445ec18377b375832bd093d3ba6
Jorys Pephily <jorys.pephily@coda-student.school>
2024-12-10 15:02:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 'use strict';
+const grid = [
+    [0, 3, 0, 0, 0, 0, 0, 0, 2, 2],
+    [0, 3, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 3, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 2, 2, 0, 0],
+    [0, 0, 0, 0, 0, 0, 5, 0, 4, 0],
+    [0, 0, 0, 0, 0, 0, 5, 0, 4, 0],
+    [0, 0, 0, 0, 0, 0, 5, 0, 4, 0],
+    [0, 3, 3, 3, 0, 0, 5, 0, 4, 0],
+    [0, 0, 0, 0, 0, 0, 5, 0, 0, 0],
+    [0, 0, 0, 0, 0, 5, 5, 5, 5, 5]
+];
 const Naval = function naval() {
     const elBody = document.querySelector('body');
     elBody.innerHTML += ``;
     let tableHTML = '<table>';
-
-    for (let i = 1; i <= 10; i += 1) {
-        tableHTML += '<tr>';
-        for (let j = 1; j <= 10; j += 1) {
-            tableHTML += `<td>${i}</td>`;
+        if (grid.length < 11 && grid[1].length < 11){
+            for (let i = 0; i < grid.length; i++) {
+                tableHTML += "<tr>";
+                for (let j = 0; j < grid[i].length; j++) {
+                    tableHTML += `<td>${grid[i][j]}</td>`; 
+                }
+                tableHTML += "</tr>";
+            }
         }
-        tableHTML += '</tr>';
-    }
-
     tableHTML += '</table>';
 
     // Ajouter le tableau dans la page
